c5c006d3584e5edc633da30430687d58a78c847e
James Macfie <james@macfie.co.nz>
2015-02-05 21:18:52 +1300

    Remove unneeded sensor edit files

--- client/views/sensors/edit/edit.js
+++
@@ -1,8 +0,0 @@
-AutoForm.addHooks('sensorItemNameEditForm', {
-	onSuccess: function(operation, result, template) {
-		Router.go('roomList');
-	},
-	onError: function(operation, error, template) {
-		console.log(error);
-	}
-});
