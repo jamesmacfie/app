2eef5f26771fdc760018feb2f7b6364518a46237
James Macfie <james@macfie.co.nz>
2015-02-05 22:17:33 +1300

    Updated homepage - needs better summary

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,12 +1,12 @@
 'use strict';
 
 Template.home.helpers({
-    belongsToNetworks: function() {
-        return !!Networks.find().count();
-    },
-    rooms: function() {
-        return Rooms.find({
-            'homepage.show': true
-        });
-    }
+		belongsToNetworks: function() {
+				return !!Networks.find().count();
+		},
+		rooms: function() {
+				return Rooms.find({
+						'homepage.show': true
+				});
+		}
 });
