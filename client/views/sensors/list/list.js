ae2edec8246c02af1ae5f58743a3e055c8842cb5
James Macfie <james@macfie.co.nz>
2015-03-24 14:03:18 +1300

    Buttons

--- client/views/sensors/list/list.js
+++ client/views/sensors/list/list.js
@@ -5,3 +5,9 @@ Template.sensorList.helpers({
 				return Sensors.find().count();
 		}
 });
+
+Template.sensorList.events({
+	'click .js-addSensor': function() {
+		console.log('add a new sensor');
+	}
+});
