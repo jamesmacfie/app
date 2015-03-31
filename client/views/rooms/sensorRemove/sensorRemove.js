e5d98b79b9af4bca1c88693fc5d74e63368a2f76
James Macfie <james@macfie.co.nz>
2015-04-01 10:27:23 +1300

    Start of room sensor removal

---
+++ client/views/rooms/sensorRemove/sensorRemove.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Template.roomSensorRemove.helpers({
+
+});
+
+Template.roomSensorRemove.events({
+	'click .js-cancelRemove': function(event, view) {
+
+	},
+	'click .js-confirmRemove': function(event, view) {
+
+	}
+});
