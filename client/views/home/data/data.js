8160abda2cb3c3e0a8947a1d76bdc925fc36aaf2
James Macfie <james@macfie.co.nz>
2015-04-11 22:05:29 +1200

    Fixing home page add sensor bug[

---
+++ client/views/home/data/data.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Template.homeData.helpers({
+	tabs: function() {
+		return [
+			{
+				name: 'Sensors',
+				slug: 'sensors'
+			},
+			{
+				name: 'Rooms',
+				slug: 'rooms'
+			}
+		];
+	},
+	belongsToNetworks: function() {
+		return !!Networks.find().count();
+	},
+	hasRooms: function() {
+		// What am I doing with the 'show on homepage' flag?
+		return Rooms.find().count();
+	},
+	hasSensors: function() {
+		// Do we have a'show on homepage' flag?
+		return Sensors.find().count();
+	}
+});
--- client/views/home/home/home.js
+++ client/views/home/home/home.js
@@ -1,30 +1,6 @@
 'use strict';
 
-Template.home.onCreated(function () {
-	this.subscribe('userRooms');
-	this.subscribe('userHubSensors');
-});
-
 Template.home.helpers({
-	tabs: function() {
-		return [
-			{
-				name: 'Sensors',
-				slug: 'sensors'
-			},
-			{
-				name: 'Rooms',
-				slug: 'rooms'
-			}
-		];
-	},
-	belongsToNetworks: function() {
-		return !!Networks.find().count();
-	},
-	hasRooms: function() {
-		// What am I doing with the 'show on homepage' flag?
-		return Rooms.find().count();
-	},
 	hasSensors: function() {
 		// Do we have a'show on homepage' flag?
 		return Sensors.find().count();
