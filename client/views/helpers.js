e96cc90f6e119a132411a1af57c04270f1fad247
James Macfie <james@macfie.co.nz>
2015-04-13 07:44:57 +1200

    Remove unused helper and change files to reference tabs

--- client/views/helpers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Template.registerHelper('pageTitle', function() {
-	return this.title;
-});
-
-ReactiveTabs.createInterface({
-	template: 'autumnTabs'
-});
---
+++ client/views/tabs.js
@@ -0,0 +1,5 @@
+'use strict';
+
+ReactiveTabs.createInterface({
+	template: 'autumnTabs'
+});
